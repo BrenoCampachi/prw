a14103e82203c3e6ad1ca0ab8e805a6d6b1962fb
breno <breno.campachi@aluno.ifsp.edu.br>
2022-03-23 10:50:06 -0300

    teste

---
+++ aula5/ex2.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+<meta charset=”UTF-8”/>
+
+<title>Breno - PHP</title>
+
+</head>
+<body>
+<h1>Lista de Exercicios</h1>
+
+<?php
+
+    $n1 = 7;
+    $n2 = 4;
+
+    $media = ($n1 + $n2)/2;
+    if($media > 7)
+    {
+        echo "Você foi aprovado! ";
+    }    
+    else
+    {
+        echo "Você foi reprovado!";
+    }
+
+?>
+</body>
+</html>
