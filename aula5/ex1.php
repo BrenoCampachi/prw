fe5ca63d9b859ff844850070c447154aed25964f
breno <breno.campachi@aluno.ifsp.edu.br>
2022-03-23 10:42:00 -0300

    exemplo if

---
+++ aula5/ex1.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+<meta charset=”UTF-8”/>
+
+<title>Breno - PHP</title>
+
+</head>
+<body>
+<h1>Lista de Exercicios</h1>
+
+<?php
+
+    $n1 = 7;
+    $n2 = 4;
+
+    $media = ($n1 + $n2)/2;
+    if($media > 7)
+    {
+        echo "Você foi aprovado! ";
+    }    
+
+?>
+</body>
+</html>
