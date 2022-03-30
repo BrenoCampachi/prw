dc67d1880f94dcb6fd973264ec7f0cd6a4425a1d
BrenoCampachi <breno.campachi@aluno.ifsp.edu.br>
2022-03-30 11:06:22 -0300

    Exercicio 1 da lista 2

---
+++ lista2/ex1.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+<meta charset=”UTF-8”/>
+
+<title>Breno - PHP</title>
+
+</head>
+<body>
+<h1>Lista de Exercicios</h1>
+
+<?php
+
+    for($repeticao=1;$repeticao<100;$repeticao++)
+    {
+        if($repeticao % 2 == 0)
+        {
+            $par = $par + $repeticao;
+        }
+
+    }
+
+    $soma = $par + $par;
+    echo $par;
+ 
+?>
+</body>
+</html>
