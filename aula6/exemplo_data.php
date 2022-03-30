c28b5a8bfca40360ec7990a1105b1a46569e6700
BrenoCampachi <breno.campachi@aluno.ifsp.edu.br>
2022-03-30 09:09:59 -0300

    teste

---
+++ aula6/exemplo_data.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+<meta charset=”UTF-8”/>
+
+<title>Breno - PHP</title>
+
+</head>
+<body>
+<h1>Aula da semana 6</h1>
+
+<?php
+    
+    echo "Data atual" .date("d/m/y");
+    
+?>
+</body>
+</html>
