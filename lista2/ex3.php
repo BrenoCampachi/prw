e3ee3786186964892f3cae8a5903db209f6eb9b0
BrenoCampachi <breno.campachi@aluno.ifsp.edu.br>
2022-03-30 11:30:01 -0300

    3

---
+++ lista2/ex3.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+<meta charset=”UTF-8”/>
+
+<title>Breno - PHP</title>
+
+</head>
+<body>
+<h1>Lista de Exercicios - 2</h1>
+
+<?php
+
+    for($i=1;$i<=200;$i++)
+    {
+        echo "O número é: " .$repeticao;
+        echo "<br>";
+
+        if( ($numero/$i)%$i != 0 ){
+            echo "$numero é primo";
+          }else{
+            echo "$numero não é primo";
+          }
+        
+    }
+ 
+?>
+</body>
+</html>
