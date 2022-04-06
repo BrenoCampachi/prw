a1b16596fcd45b50bd44e292b5e903c3ded05733
breno <breno.campachi@aluno.ifsp.edu.br>
2022-04-06 09:49:34 -0300

    Exercicios - Lista 3

---
+++ lista3/ex1.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+<meta charset=”UTF-8”/>
+
+<title>Breno - PHP</title>
+
+</head>
+<body>
+<h1>Lista de Exercicios - 3</h1>
+
+<?php
+
+    $palavra = "Paralelepipedo";
+    echo "A variável contém: " . strlen($palavra) . "caracteres" . "<br>";
+    $palavra = trim($palavra);
+    echo "A variável agora contém: " . strlen($palavra) . "caracteres";
+ 
+?>
+</body>
+</html>
