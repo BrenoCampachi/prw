dc71e473e7bdb943c1f01acb8ee7c34a32caaaf9
breno <breno.campachi@aluno.ifsp.edu.br>
2022-04-09 16:32:58 -0300

    lista3

--- lista3/ex2.php
+++ lista3/ex2.php
@@ -1,12 +1,29 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+<meta charset=”UTF-8”/>
+
+<title>Breno - PHP</title>
+
+</head>
+<body>
+<h1>Lista de Exercicios - 3</h1>
+
 <?php
     $vogais = array("a", "e", "i", "o", "u", "A", "E", "I", "O", "U");
     
-    $palavra = "Estudantes" ;
+    $palavra = "Funcionarios" ;
 
     $nova_palavra = str_replace($vogais, "x", $palavra);
     
     echo "Palavra: ". $palavra. "<br><br>" ;
 
-    echo "Nova palavra: ". $nova_palavra ;
+    echo "Nova palavra é: ". $nova_palavra ;
 
 ?>
+
+</body>
+</html>
