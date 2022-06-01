133a36227c24ab48848b311938ca9702756703cd
breno <breno.campachi@aluno.ifsp.edu.br>
2022-06-01 11:55:54 -0300

    commit

---
+++ agenda2/insert_agenda.sql
@@ -0,0 +1,6 @@
+USE agenda01;
+
+INSERT INTO agenda (nome, apelido, endereco, bairro, cidade, estado, telefone, celular, email, data_cadastro)
+VALUES ('Breno Campachi', 'Xama', 'Rua Barão', 'Centro', 'Brauna', 'SP', '18996559663', '36921890', 'brenohvc27@gmail.com', '2022/09/12');
+
+SELECT * FROM agenda;
---
+++ agenda2/insert_agenda2.sql
@@ -0,0 +1,6 @@
+USE agenda02;
+
+INSERT INTO agenda2 (nome, apelido, endereco, bairro, cidade, estado, telefone, celular, email, data_cadastro)
+VALUES ('Breno Campachi', 'Xama', 'Rua Barão', 'Centro', 'Brauna', 'SP', '18996559663', '36921890', 'brenohvc27@gmail.com', '2022/09/12');
+
+SELECT * FROM agenda2;
